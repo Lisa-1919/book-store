da89b8d4af230bc8302eeb9245fe18ec2583150d
Lisa-1919 <lizasimerova438@gmail.com>
2023-03-28 23:34:23 +0300

    create project

---
+++ src/main/java/com/bookstoreversion2/BookStoreVersion2Application.java
@@ -0,0 +1,13 @@
+package com.bookstoreversion2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreVersion2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreVersion2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookstoreversion2/BookStoreVersion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstoreversion2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreVersion2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
