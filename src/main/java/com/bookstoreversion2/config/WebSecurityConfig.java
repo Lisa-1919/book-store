39db261870757beb81265fa383cad48fd1a09850
Lisa-1919 <lizasimerova438@gmail.com>
2023-03-28 23:36:59 +0300

    add configuration classes

---
+++ src/main/java/com/bookstoreversion2/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.bookstoreversion2.config;
+
+import com.bookstoreversion2.services.UserServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private UserServiceImp userServiceImp;
+
+    @Value("${spring.security.debug:false}")
+    private boolean securityDebug;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/registration").permitAll()
+                        .requestMatchers("/home").permitAll()
+                        .requestMatchers("/admin/*").hasRole("ADMIN")
+                        .requestMatchers("/manager/*").hasRole("MANAGER")
+                        .requestMatchers("/user/*").hasRole("USER")
+                        .requestMatchers("/account").fullyAuthenticated()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userServiceImp).passwordEncoder(bCryptPasswordEncoder);
+    }
+}
